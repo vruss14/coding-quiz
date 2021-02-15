ea95da639928a61f4cf3d56d38606908f82b0016
Valerie Russell <vruss14@gmail.com>
2021-02-15 18:33:33 -0500

    added and styled a timer that begins when the start button is clicked

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Testing the page
 console.log("Page loaded!");
+
+// Timer
+
+let startButton = document.querySelector("#start-button");
+let timeLeft = document.querySelector(".time-left");
+let secondsRemaining = 60;
+
+startButton.addEventListener("click", function setTimer() {
+    let timerInterval = setInterval(function() {
+        secondsRemaining--;
+        timeLeft.textContent = "Time left: " + secondsRemaining;
+
+        if(secondsRemaining === 0) {
+            clearInterval(timerInterval);
+        }
+        
+    }, 1000);
+});
