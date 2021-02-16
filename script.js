4adc017189f4b8adddf278e5f5edddfadc098a0a
Valerie Russell <vruss14@gmail.com>
2021-02-16 17:41:39 -0500

    created the script to transition between two questions and added padding to question text

--- script.js
+++ script.js
@@ -24,6 +24,10 @@ startButton.addEventListener("click", function setTimer() {
         secondsRemaining--;
         timeLeft.textContent = "Time left: " + secondsRemaining;
 
+        if(secondsRemaining <=10) {
+            timeLeft.setAttribute("style", "color: red");
+        }
+
         if(secondsRemaining === 0) {
             clearInterval(timerInterval);
         }
@@ -33,6 +37,7 @@ startButton.addEventListener("click", function setTimer() {
 
 //Question one
 
+let userScore = 0;
 let homeInfo = document.querySelector("#quiz-heading");
 let questionHeader = document.querySelector ("#question-text");
 
@@ -42,8 +47,9 @@ let choiceC = document.querySelector ("#optionc");
 let choiceD = document.querySelector ("#optiond");
 
 let firstQuestion = "Which programming language is commonly responsible for the advanced functionality of a webpage?";
+let secondQuestion = "What does HTML stand for?";
 
-startButton.addEventListener("click", function runQuestions() {
+startButton.addEventListener("click", function runQuestionOne() {
     homeInfo.setAttribute ("style", "display: none");
     questionHeader.textContent = firstQuestion;
     optiona.textContent = "HTML";
@@ -52,3 +58,58 @@ startButton.addEventListener("click", function runQuestions() {
     optiond.textContent = "Python";
 
 });
+
+// User answers question one
+
+function userResponseOne (event) {
+    if (event.target.id == "optiona") {
+        secondsRemaining = secondsRemaining - 15;
+        runQuestionTwo ();
+    } else if (event.target.id == "optionb") {
+        secondsRemaining = secondsRemaining - 15;
+        runQuestionTwo ();
+    } else if (event.target.id == "optionc") {
+        userScore = userScore + 10;
+        console.log(userScore);
+        runQuestionTwo ();
+
+    } else {
+        secondsRemaining = secondsRemaining - 15;
+        questionHeader.textContent = secondQuestion;
+        runQuestionTwo ();
+    }
+}
+
+choiceA.addEventListener("click", userResponseOne);
+choiceB.addEventListener("click", userResponseOne);
+choiceC.addEventListener("click", userResponseOne);
+choiceD.addEventListener("click", userResponseOne);
+
+// Question two
+
+function runQuestionTwo () {
+    questionHeader.textContent = secondQuestion;
+    optiona.textContent = "Hypertext Markup Language";
+    optionb.textContent = "Hypertext Markdown Language";
+    optionc.textContent = "Hyperlinks Meta tags and Loops";
+    optiond.textContent = "Hypertext Markup Lingo";
+}
+
+function userResponseTwo (event) {
+    if (event.target.id == "optiona") {
+        userScore = userScore + 10;
+        console.log(userScore);
+    /* } else if (event.target.id == "optionb") {
+        choiceB.setAttribute("style", "background-color:red");
+    } else if (event.target.id == "optionc") {
+        choiceC.setAttribute("style", "background-color:red");
+    } else {
+        choiceD.setAttribute("style", "background-color:red"); */
+    }
+}
+
+choiceA.addEventListener("click", userResponseTwo);
+choiceB.addEventListener("click", userResponseTwo);
+choiceC.addEventListener("click", userResponseTwo);
+choiceD.addEventListener("click", userResponseTwo);
+
